4eb16be7ddd8840829b2b98169dcd3acc6ddc7c1
Zeeshan <sk.zeeshan1992@gmail.com>
2020-09-28 20:54:38 -0400

    configure localization

--- frontend/src/AppContextProvider.tsx
+++ frontend/src/AppContextProvider.tsx
@@ -1,7 +1,9 @@
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import React from 'react';
+import { I18nextProvider } from 'react-i18next';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { i18n } from './i18n/i18n';
 import { store } from './redux';
 import { theme } from './theme/muiTheme';
 
@@ -12,7 +14,9 @@ const AppContextProvider: React.FC<AppContextProviderProps> = ({ children }) => 
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Provider store={store}>
-        <BrowserRouter>{children}</BrowserRouter>
+        <I18nextProvider i18n={i18n}>
+          <BrowserRouter>{children}</BrowserRouter>
+        </I18nextProvider>
       </Provider>
     </ThemeProvider>
   );
---
+++ frontend/src/i18n/i18n.ts
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+import { en, fr } from '.';
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: process.env.NODE_ENV === 'development' ? true : false,
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en,
+      fr,
+    },
+  });
+
+export { i18n };
